4b739165f0ef35593bb039f9dff34ac69619554c
Arnav Jain <arnavjain92105@gmail.com>
2024-12-14 15:50:49 +0530

    p7

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -69,7 +69,7 @@ const Detail = ({x,Setx,xbg}) => {
             <div className={`hover:scale-105 rounded-xl transition-all duration-500 ease-in-out shadow-2xl w-[500px] h-[270px] bg-cover bg-center`}
             style={
               {
-                backgroundImage:`url(/public${details[xbg].image})`,
+                backgroundImage:`url(${details[xbg].image})`,
                 backgroundSize:'cover',
                 backgroundPosition:'center',
               }
